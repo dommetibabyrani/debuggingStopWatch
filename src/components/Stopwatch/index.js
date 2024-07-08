d30896d7630f04b5c3a47e14c7b3b4d37acceb9d
dommetibabyrani <160019313+dommetibabyrani@users.noreply.github.com>
2024-07-08 13:09:08 +0530

    index.js

--- src/components/Stopwatch/index.js
+++ src/components/Stopwatch/index.js
@@ -9,11 +9,14 @@ class Stopwatch extends Component {
   }
   // FIX 1: The spelling of this life cycle method should be componentWillUnmount
   componentWillUnmount() {
+    // FIX 2: When the setInterval method is used, it is best practice to use clearInterval method to clear the schedular
     clearInterval(this.timeInterval)
   }
 
   onResetTimer = () => {
+    // FIX 3: class variables should be accessed using "this" keyword
     clearInterval(this.timeInterval)
+    // FIX 4: The timer should be reset to 0 when the "Reset" button is clicked
     this.setState({isTimerRunning: false, timeElapsedInSeconds: 0})
   }
 
@@ -24,11 +27,15 @@ class Stopwatch extends Component {
 
   updateTime = () => {
     this.setState(prevState => ({
+      // FIX 5: The key timeElapsedInSeconds in prevState should be used to update the state
       timeElapsedInSeconds: prevState.timeElapsedInSeconds + 1,
     }))
   }
 
   onStartTimer = () => {
+    // FIX 6: Here to update the timeElapsedInSeconds value for every 1000 milliseconds setInterval method should be used
+    // FIX 7: The id returned by setinterval should be stored in a class variable to access it across other methods in the class
+    // FIX 8: class methods should be accessed using "this keyword"
     this.timeInterval = setInterval(this.updateTime, 1000)
     this.setState({isTimerRunning: true})
   }
@@ -55,6 +62,7 @@ class Stopwatch extends Component {
 
   render() {
     const {isTimerRunning} = this.state
+    // FIX 9: class methods should be accessed using "this keyword"
     const time = `${this.renderMinutes()}:${this.renderSeconds()}`
 
     return (
@@ -102,4 +110,5 @@ class Stopwatch extends Component {
   }
 }
 
+// FIX 10: The syntax for default export should be written like this
 export default Stopwatch
